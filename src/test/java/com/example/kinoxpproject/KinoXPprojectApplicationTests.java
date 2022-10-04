702b155082012d45f49be53ad5f4a1f76ab8a63e
Simon <simonffsss@gmail.com>
2022-10-04 08:52:45 +0200

    Initial commit

---
+++ src/test/java/com/example/kinoxpproject/KinoXPprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinoxpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXPprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
