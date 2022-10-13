25bcd6215fa6b490e26ae7a94f5de1deaacf3bba
asgerbirk <asger.birk@gmail.com>
2022-10-13 10:26:39 +0200

    problemer med test

---
+++ src/main/java/com/example/kinoxpproject/CORSFilter.java
@@ -0,0 +1,39 @@
+package com.example.kinoxpproject;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// Kilde: https://www.angularfix.com/2022/04/cors-with-spring-boot-and-angularjs-not.html
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+
+    @Override
+    public void destroy() { }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Auth-Token, Content-Type, token");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if(!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException { }
+}
